1d417071f95c2c330c49ec2db397281abe4d6ea5
Max <sem4enko_max@ukr.net>
2020-12-29 16:27:09 +0200

    commit

--- src/Task1.java
+++ src/Task1.java
@@ -5,32 +5,22 @@ public class Task1 {
         markGenerator();
     }
 
-    private static void markGenerator() {
+    private static void markGenerator( ) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input mark:");
         Integer num = scanner.nextInt();
 
 
-        switch (num) {
-            case 0, 1, 2, 3, 4:
-                System.out.println("Very Bad");
-                break;
-            case 5:
-                System.out.println("Bad");
-                break;
-            case 6:
-                System.out.println("Accepted");
-                break;
-            case 7, 8:
-                System.out.println("Good");
-                break;
-            case 9, 10:
-                System.out.println("Well");
-                break;
-            default:
-                System.out.println("Please input marks from 0 to 10");
-                markGenerator();
-        }
+        String textMark = switch (num) {
+            case 9, 10 -> "Well";
+            case 7, 8 -> "Good";
+            case 6 -> "Acceptable";
+            case 5 -> "Bad";
+            case 1, 2, 3, 4 -> "Very Bad";
+            default -> "Please input marks from 0 to 10 ";
+
+        };
+        System.out.println(textMark);
     }
 }
 
